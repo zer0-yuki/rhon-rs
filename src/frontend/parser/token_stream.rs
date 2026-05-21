943f7d7168a485866a98d711e9d0936d4b4382dc
0yuki <147019313+Aoyukiii@users.noreply.github.com>
2026-05-21 18:41:30 +0800

    fix: add struct

--- src/frontend/parser/token_stream.rs
+++ src/frontend/parser/token_stream.rs
@@ -4,3 +4,30 @@ pub trait TokenStream {
     fn advance(&mut self) -> Token;
     fn current(&self) -> &Token;
 }
+
+pub struct SliceStream<'tok> {
+    tokens: &'tok [Token],
+    pos: usize,
+}
+
+impl<'tok> SliceStream<'tok> {
+    pub fn new(tokens: &'tok [Token]) -> Self {
+        Self { tokens, pos: 0 }
+    }
+}
+
+impl<'tok> TokenStream for SliceStream<'tok> {
+    fn advance(&mut self) -> Token {
+        let cur = self.current().clone(); // TODO: clone is not good
+        self.pos += 1;
+        cur
+    }
+
+    fn current(&self) -> &Token {
+        if self.pos >= self.tokens.len() {
+            &Token::EOF
+        } else {
+            &self.tokens[self.pos]
+        }
+    }
+}
